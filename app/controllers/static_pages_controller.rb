7549b5c74bffdb7d61c3e22158cea7062204c639
André Baudin-Laurencin <abaudin@gmail.com>
2014-10-23 12:08:31 +0200

    Finish static pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,13 @@
 class StaticPagesController < ApplicationController
   def home
+    @title = 'Home'
   end
 
   def help
+    @title = 'help'
+  end
+
+  def about
+    @title = 'about'
   end
 end
